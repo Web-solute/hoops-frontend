8c00cb661b8d2170d22d444d29f4b56e43f11344
sinbox0701 <psh090953@gmail.com>
2022-01-09 20:52:51 +0900

    예약 리스트 받기 & 예약 삭제

--- src/Components/Modal/MypageModal.js
+++ src/Components/Modal/MypageModal.js
@@ -1,9 +1,40 @@
 import { Background, Window, Popup } from "./Modal";
 import { Flex, Absolute, Subtitle, Submitbutton } from "../shared";
 import cancel from '../../images/cancel.png';
+import { gql, useMutation } from "@apollo/client";
+
+
+const DELETE_RESERVATION_MUTATION = gql`
+    mutation deleteReservation($id:Int!){
+        deleteReservation(id:$id){
+            ok
+            error
+        }
+    }
+`;
 
 const MypageModal = (props) => {
-
+    const [deleteReservation,{loading}] = useMutation(DELETE_RESERVATION_MUTATION);
+    const onDeleteClick = () => {
+        const id = Number(props.id);
+        const deleteReservationUpdate = (cache,result) => {
+            const {
+                data:{
+                    deleteReservation:{ok}
+                } 
+            } = result;
+            if(ok){
+                cache.evict({id:`Reservation:${id}`})
+            }
+        };
+        deleteReservation({
+            variables:{
+                id
+            },
+            update:deleteReservationUpdate
+        });
+        props.setAction();
+    }
     return (
         <Background>
             <Window>
@@ -11,7 +42,7 @@ const MypageModal = (props) => {
                 <Flex padding='20px'>
                     <Absolute right='15px'><img onClick={ ()=>{ props.setModal(false) }} src={cancel} alt='cancel'/></Absolute>
                     <Subtitle size='26px' top='55px'>정말로 {props.setText} <br/> 하시겠습니까?</Subtitle>
-                    <Submitbutton onClick={()=> props.setAction() } type="submit" value= '확인' mt='20px' height='45px' />
+                    <Submitbutton onClick={()=> onDeleteClick() } type="submit" value= '확인' mt='20px' height='45px' />
                 </Flex>
                 </Popup>
             </Window>
