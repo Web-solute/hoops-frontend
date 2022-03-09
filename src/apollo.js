8c40307b4490c9ae600f959fb56dcccbc412eb63
sinbox0701 <psh090953@gmail.com>
2022-03-09 15:58:14 +0900

    apollo 추가

--- src/apollo.js
+++ src/apollo.js
@@ -26,7 +26,7 @@ const authLink = setContext((_,{headers}) => {
 
 export const client = new ApolloClient({
     link: authLink.concat(createUploadLink({
-        uri:"http://localhost:4000/graphql"
+        uri:process.env.NODE_ENV === "prodcution" ? "https://hufshoops.herokuapp.com/graphql" : "http://localhost:4000/graphql"
     })),
     cache: new InMemoryCache()
 });
